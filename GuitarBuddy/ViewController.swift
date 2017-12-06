a5d134afbb5fb6f6e1e7f9f1baac2ad3a8014dde
Brad Siegel <brad.siegel@spr.com>
2017-12-06 12:30:47 -0600

    Initial Commit

---
+++ GuitarBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuitarBuddy
+//
+//  Created by Brad Siegel on 12/6/17.
+//  Copyright © 2017 Brad Siegel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
